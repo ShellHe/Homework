935253f5b4f453d80ca30c416468e78ee2d847cd
Shell He <she2019@kewforest.org>
2017-12-14 00:13:56 -0500

    Initial commit

---
+++ 2.py
@@ -0,0 +1,13 @@
+#Clockwise
+def turn_clockwise(he):
+    if he ==("North"):
+        return print("East")
+    elif he == ("East"):
+        return print("South")
+    elif he == ("South"):
+        return print("West")
+    elif he == ("West"):
+        return print("North")
+    else:
+        print("lt is not defined")
+
