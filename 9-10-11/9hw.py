31602ed0bc9b99afc92e77ac88e4b60adf2a4029
Shell He <she2019@kewforest.org>
2018-04-04 14:36:08 -0400

    wow

---
+++ 9-10-11/9hw.py
@@ -0,0 +1,16 @@
+tuplist=("Julia", "Roberts", 1967, "Duplicity", 2009, "Actress", "Atlanta, Georgia")
+
+def f(tup):
+    print(tup[0])
+
+f(tuplist)
+
+#YES it is possible
+
+#Is a pair a generalization of a tuple, or is a tuple a generalization of a pair?
+
+#A tuple is a generlization of a pair
+
+# Is a pair a kind of tuple, or is a tuple a kind of pair?
+
+#A pair is a kind of tuple
---
+++ 9-10-11/chapter9.py
@@ -0,0 +1,9 @@
+def test_tuple(mytuple=(3,2)):
+    return mytuple
+thistuple=(1,2)
+print(test_tuple((1,2)))
+
+mytuple =("alli","wison")
+mytuple(0)="jaxsin"
+
+print(mytuple(0))
